7334d484aea085a320030d700fa989024a7f80db
Joya322 <jannatferdoush1212@gmail.com>
2025-09-03 12:01:36 +0600

    first commit

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    // load data
+    useEffect(() => {
+        fetch("blogs.json").then(res => res.json()).then(data=> setBlogs(data))
+    }, [])
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
